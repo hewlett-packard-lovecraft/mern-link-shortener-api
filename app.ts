6dd5285ae0f2b6b5752c281c9dddde4304f3917e
Haoming Xia <mrmist3r311@gmail.com>
2021-03-19 06:28:17 +0000

    .

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
 
@@ -7,10 +7,19 @@ import rateLimit from "express-rate-limit";
 // routes here
 import Main from "./routes/Main";
 
-const router = express.Router();
-const app = express();
 
+const app = express();
 app.use(morgan("dev"));
 app.listen(3000);
 
 app.get("/", Main);
+
+// set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// get the default connection
+var db = mongoose.connection;
+
+// bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
